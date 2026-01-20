37ff76eb634dc8bae275efa54d85bb96e17f1e43
nathano <nathan@onraet.be>
2026-01-20 21:05:07 +0100

    Removed Hardcoded Admin ID And Changed Admin Role to 'lettervandedag_overheid'

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -21,7 +21,7 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
 					id: profile.id,
 					zauthId: profile.id,
 					username: profile.username,
-					admin: profile.roles.includes('lettervandedag_manager') || profile.id == 391, // TODO: Remove
+					admin: profile.roles.includes('lettervandedag_overheid'),
 					picture: profile.picture
 				};
 			}
