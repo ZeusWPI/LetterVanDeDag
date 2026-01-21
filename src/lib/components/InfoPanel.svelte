33e6942aad0974f60548b0b51fdfc5328a71ef42
nathano <nathan@onraet.be>
2026-01-21 18:15:42 +0100

    Made Date Display in InfoPanel Not ISO string anymore

--- src/lib/components/InfoPanel.svelte
+++ src/lib/components/InfoPanel.svelte
@@ -8,7 +8,7 @@
 
 {#if selectedLetter}
 	<div class="border-2  p-3 border-gray-100 dark:border-zinc-700 flex flex-col rounded-lg lg:mt-10">
-		<span class="text-xl font-bold mb-2">{selectedLetter.created_at}</span>
+		<span class="text-xl font-bold mb-2">{new Date(selectedLetter.created_at).toDateString()}</span>
 		<span><b class="mr-2">Letter:</b>{selectedLetter.letter}</span>
 		<span class="flex flex-row"><b class="mr-2">Declared by:</b><img class="size-6 rounded"
 																																		 src="https://zpi.zeus.gent/image/{selectedLetter.added_by.id}"
