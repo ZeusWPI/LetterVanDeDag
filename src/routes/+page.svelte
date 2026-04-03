a6be9203710c5f29ed7ba70bd7ff9cb0e025a4f7
Hannes Klinckaert <hannes-dev@mailbox.org>
2026-04-03 11:50:09 +0200

    fix mobile gap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,15 +14,16 @@
 
 <div class="flex w-full flex-col items-center gap-10">
 	<div
-		class="grid w-full px-5 lg:px-0 lg:w-5/7 grid-cols-1 lg:grid-cols-4 items-center justify-center gap-6 lg:items-start">
-		<div class="lg:mt-15 row-start-3 lg:row-start-1">
+		class="grid w-full grid-cols-1 items-center justify-center gap-y-6 px-5 lg:w-5/7 lg:grid-cols-4 lg:items-start lg:gap-6 lg:px-0"
+	>
+		<div class="row-start-3 lg:row-start-1 lg:mt-15">
 			<Leaderboard leaderboard_users={data.leaderboard_users} />
 		</div>
 		<div class="col-span-2">
 			<Calendar bind:selectedDate letterData={data.letters} />
 		</div>
 		{#if selectedLetter || user?.admin}
-			<div class="flex flex-col gap-4 row-start-2 lg:row-start-1 lg:col-start-4 lg:mt-15">
+			<div class="row-start-2 flex flex-col gap-4 lg:col-start-4 lg:row-start-1 lg:mt-15">
 				<InfoPanel {selectedLetter} {selectedDate} {user} declarers={data.declarers} />
 			</div>
 		{/if}
