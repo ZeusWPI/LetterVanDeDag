3aeca49f299d8c14b93b9c8d665ab9738adfdda2
Hannes Klinckaert <hannes-dev@mailbox.org>
2026-04-03 11:56:23 +0200

    make calendar wider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,16 +14,16 @@
 
 <div class="flex w-full flex-col items-center gap-10">
 	<div
-		class="grid w-full grid-cols-1 items-center justify-center gap-y-6 px-5 lg:w-5/7 lg:grid-cols-4 lg:items-start lg:gap-6 lg:px-0"
+		class="grid w-full grid-cols-1 items-center justify-center gap-y-6 px-5 lg:w-6/7 lg:grid-cols-5 lg:items-start lg:gap-6 lg:px-0"
 	>
 		<div class="row-start-3 lg:row-start-1 lg:mt-15">
 			<Leaderboard leaderboard_users={data.leaderboard_users} />
 		</div>
-		<div class="col-span-2">
+		<div class="col-span-3">
 			<Calendar bind:selectedDate letterData={data.letters} />
 		</div>
 		{#if selectedLetter || user?.admin}
-			<div class="row-start-2 flex flex-col gap-4 lg:col-start-4 lg:row-start-1 lg:mt-15">
+			<div class="row-start-2 flex flex-col gap-4 lg:col-start-5 lg:row-start-1 lg:mt-15">
 				<InfoPanel {selectedLetter} {selectedDate} {user} declarers={data.declarers} />
 			</div>
 		{/if}
