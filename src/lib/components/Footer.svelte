b5baa16c3416bf0d751db2a6682d8dd0545571b5
Hannes Klinckaert <hannes-dev@mailbox.org>
2026-04-05 23:37:56 +0200

    style improvements

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,11 +3,17 @@
 	import SvelteIcon from '$lib/assets/svelte.svg';
 </script>
 
-<div class="w-full flex flex-row justify-between px-6 mb-3">
+<div class="mt-10 mb-3 flex w-full flex-row justify-between px-6">
 	<a href="https://github.com/ZeusWPI/LetterVanDeDag" class="hover:underline">
 		<img class="inline size-4" src={SvelteIcon} alt="Svelte" />
 		v{PUBLIC_APP_VERSION}
 	</a>
-	<span>Made with ❤️ by <img class="size-8 ml-1 inline" src="https://zinc.zeus.gent/zeus" alt="Zeus"></span>
+	<span
+		>Made with ❤️ by <img
+			class="ml-1 inline size-8"
+			src="https://zinc.zeus.gent/zeus"
+			alt="Zeus"
+		/></span
+	>
 	<span></span>
 </div>
