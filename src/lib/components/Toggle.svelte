13ace2293b28ff64dbf5cadf4ebcd924adcea079
nathano <nathan@onraet.be>
2026-07-26 19:31:04 +0200

    Added Support for Adding New Letters As Duplicate of Old Letters

---
+++ src/lib/components/Toggle.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	let { value = $bindable(false) } = $props();
+</script>
+
+<label class="switch">
+	<input type="checkbox" bind:checked={value} />
+	<span class="slider round"></span>
+</label>
+
+<style>
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 40px;
+        height: 22px;
+    }
+
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        inset: 0;
+        background-color: #ccc;
+        transition: 0.2s;
+    }
+
+    .slider:before {
+        position: absolute;
+        content: "";
+        height: 16px;
+        width: 16px;
+        left: 3px;
+        bottom: 3px;
+        background-color: white;
+        transition: 0.2s;
+    }
+
+    input:checked + .slider {
+        background-color: var(--color-orange);
+    }
+
+    input:focus + .slider {
+        box-shadow: 0 0 1px var(--color-orange);
+    }
+
+    input:checked + .slider:before {
+        transform: translateX(18px);
+    }
+
+    .slider.round {
+        border-radius: 22px;
+    }
+
+    .slider.round:before {
+        border-radius: 50%;
+    }
+</style>
--- src/routes/new_letter/+server.ts
+++ src/routes/new_letter/+server.ts
@@ -7,56 +7,72 @@ import { v4 as uuidv4 } from 'uuid';
 
 export async function POST({ request, locals }): Promise<void> {
 	const session = await locals.auth();
-	if (session && (session.user as ZAuthUser)?.admin) {
-		const data = await request.formData();
+	if (!(session && (session.user as ZAuthUser)?.admin)) {
+		return error(401, 'Unauthorized');
+	}
 
-		const date = data.get('created_at') as string | null;
-		const letter = data.get('letter') as string | null;
-		const image = data.get('image') as File | null;
-		const declarerId = data.get('declarer') as string | null;
+	const data = await request.formData();
 
+	const date = data.get('created_at') as string | null;
+	const duplicate = data.get('duplicate') as boolean | null;
+	const declarerId = data.get('declarer') as string | null;
+	const originalDate = data.get('original-date') as string | null; // Should be yyyy-mm-dd
+
+	let letter = data.get('letter') as string | null;
+	const image = data.get('image') as File | null;
+
+	let imageUrl: string | null = null;
+
+	if (!duplicate) {
 		if (!date || !letter || !declarerId) {
 			throw error(400, 'Not all required fields are present');
 		}
 
-		const existingLetter = getLetterByDay(date);
-
-		let imageUrl = null;
 		if (image !== null && image.size > 0) {
 			const uuid = uuidv4().replace(/-/g, '');
 			const fileExtension = image.name.substring(image.name.lastIndexOf('.'));
 			const filename = `${env.IMAGE_PATH}/${uuid}${fileExtension}`;
 			await writeFile(filename, Buffer.from(await image?.arrayBuffer()));
 			imageUrl = `/images/${uuid}${fileExtension}`;
+			}
+
+	} else {
+		if (!date || !declarerId || !originalDate) {
+			throw error(400, 'Not all required fields are present');
 		}
 
-		const result: LetterVanDeDag = {
-			letter: letter,
-			created_at: date,
-			added_by: {
-				id: Number(declarerId)
-			},
-			imageUrl: imageUrl ? imageUrl : undefined
-		};
-
-		addLetter(result);
-
-		if (existingLetter && existingLetter.imageUrl) {
-			console.log(`old image count: ${getImageLetterCount(existingLetter.imageUrl)}`);
+		const originalLetter = getLetterByDay(originalDate);
+		if (originalLetter == null) {
+			throw error(400, 'Tried to add duplicate of day without letter');
 		}
-
-		if (
-			existingLetter &&
-			existingLetter.imageUrl &&
-			getImageLetterCount(existingLetter.imageUrl) === 0
-		) {
-			console.log(
-				`Image ${existingLetter.imageUrl} has no references anymore and is being deleted.}`
-			);
-			const filename = `${env.IMAGE_PATH}/${existingLetter.imageUrl.substring('/images/'.length - 1)}`;
-			await unlink(filename);
-		}
-
-		throw redirect(303, '/');
+		letter = originalLetter.letter;
+		imageUrl = originalLetter.imageUrl ? originalLetter.imageUrl : null;
 	}
+
+	const existingLetter = getLetterByDay(date);
+
+	const result: LetterVanDeDag = {
+		letter: letter,
+		created_at: date,
+		added_by: {
+			id: Number(declarerId)
+		},
+		imageUrl: imageUrl ? imageUrl : undefined
+	};
+
+	addLetter(result);
+
+	if (
+		existingLetter &&
+		existingLetter.imageUrl &&
+		getImageLetterCount(existingLetter.imageUrl) === 0
+	) {
+		console.log(
+			`Image ${existingLetter.imageUrl} has no references anymore and is being deleted.}`
+		);
+		const filename = `${env.IMAGE_PATH}/${existingLetter.imageUrl.substring('/images/'.length - 1)}`;
+		await unlink(filename);
+	}
+
+	throw redirect(303, '/');
 }
