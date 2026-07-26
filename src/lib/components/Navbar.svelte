09218d11cd15863522dccda9278dd5343450eead
nathano <nathan@onraet.be>
2026-07-26 18:40:05 +0200

    Added Link to Mattermost Channel in Navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Logo from '$lib/assets/LetterVanDeDag.svg';
+	import MattermostIcon from '$lib/assets/mattermost.svg';
+	import { env } from '$env/dynamic/public';
 </script>
 
 <div class="w-screen h-16 mb-6 flex flex-row justify-between items-center bg-zoranje">
@@ -7,4 +9,8 @@
 		<img class="size-10 mr-2" src={Logo} alt="icon">
 		Letter van de Dag
 	</span>
+	<a class="mr-3 hover:bg-orange-600 h-full flex items-center justify-center px-4"
+	   href={env.PUBLIC_MATTERMOST_URL} target="_blank">
+		<img class="size-9 brightness-0 invert" src={MattermostIcon} alt="icon" />
+	</a>
 </div>
