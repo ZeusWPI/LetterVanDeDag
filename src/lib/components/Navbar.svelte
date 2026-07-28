6ca56f8a61d3ef39bae69d50d0a76fb4f040fac4
nathano <nathan@onraet.be>
2026-07-28 20:43:47 +0200

    Made Navbar Logo a Link to the Main Page

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -6,10 +6,10 @@
 </script>
 
 <div class="w-screen h-16 mb-6 flex flex-row justify-between items-center bg-zoranje">
-	<span class="font-black text-white text-4xl pl-3 flex flex-row">
+	<a href="/" class="font-black text-white text-4xl pl-3 flex flex-row">
 		<img class="size-10 mr-2" src={Logo} alt="icon">
 		Letter van de Dag
-	</span>
+	</a>
 	<span class="mr-3 flex flex-row justify-end h-full">
 	<a class="hover:bg-orange-600 h-full flex items-center justify-center px-4"
 	   href="/letterlist">
